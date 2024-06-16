f347140433c66e9e9b6c0aab378b5784584b8879
maysonderulo <166694887+maysonderulo@users.noreply.github.com>
2024-06-17 03:01:37 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,18 @@
-from openai import OpenAI
+import os
+import openai
 import streamlit as st
 
-
-api_key = sk-proj-yKWGbnHznn8PZbP1CAkWT3BlbkFJYxluBHJWAz70PgHY4sMC
+# 환경 변수에서 API 키 가져오기
+api_key = os.getenv("OPENAI_API_KEY")
 if api_key:
-    client = OpenAI(api_key=api_key)
+    openai.api_key = api_key
 
     if "openai_model" not in st.session_state:
         st.session_state["openai_model"] = "gpt-3.5-turbo"
 
     system_message = '''
-    너는 부경대학교 학생의 학교생활을 도와주는 친절한 챗봇 '백경이'야 질문에 답을 할때 항상 반말로 해줘. 무슨일이 있어도 반말로 해줘 
-    그리고 주어진 데이터 범위의 밖의 것들은 미안하지만 모른다고 대답해야해!.
+    너는 부경대학교 학생의 학교생활을 도와주는 친절한 챗봇 '백경이'야. 질문에 답을 할 때 항상 반말로 해줘. 무슨 일이 있어도 반말로 해줘.
+    그리고 주어진 데이터 범위 밖의 것들은 미안하지만 모른다고 대답해야 해!.
     '''
 
     if "messages" not in st.session_state:
@@ -25,21 +26,18 @@ if api_key:
             with st.chat_message(message["role"]):
                 st.markdown(message["content"])
 
-    if prompt := st.chat_input("안녕, 나는 대학 생활을 도와주는 백경이야. 뭘 도와줄까 ?"):
+    if prompt := st.chat_input("안녕, 나는 대학 생활을 도와주는 백경이야. 뭘 도와줄까?"):
         st.session_state.messages.append({"role": "user", "content": prompt})
         with st.chat_message("user"):
             st.markdown(prompt)
 
         with st.chat_message("assistant"):
-            stream = client.chat.completions.create(
+            response = openai.ChatCompletion.create(
                 model=st.session_state["openai_model"],
-                messages=[
-                    {"role": m["role"], "content": m["content"]}
-                    for m in st.session_state.messages
-                ],
-                stream=True,
-            )
-            response = st.write_stream(stream)
+                messages=st.session_state.messages,
+                stream=False
+            ).choices[0].message['content']
+            st.markdown(response)
         st.session_state.messages.append({"role": "assistant", "content": response})
 
     if st.button("Clear"):
@@ -48,8 +46,7 @@ if api_key:
     if st.button("Exit Chat"):
         del st.session_state.messages
 else:
-    st.info("API key를 입력해주세요.")
-
+    st.info("API 키를 환경 변수로 설정해주세요.")
 
 interpreter_code = st.text_area("Code Interpreter")
 
